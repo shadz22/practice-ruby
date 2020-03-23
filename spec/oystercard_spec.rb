29a586725b83182d4150bbda40e2209aefd09ab7
shadz22 <shadz22@yahoo.co.uk>
2020-03-23 16:52:12 +0000

    Raise error if maximum balance is reached when toping up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,6 +14,13 @@ describe Oystercard do
     it 'tops ups' do
       expect{ subject.top_up(1) }.to change{ subject.balance }.by 1
     end
+
+    it 'throws an error if maximum balance is reached' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up(1)}.to raise_error 'Maximum balance of #{MAXIMUM_BALANCE} exceeded'
+    end
+
   end
     
 end
