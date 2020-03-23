fbfea1b88480f26c5171d3358cb74e7957015a28
shadz22 <shadz22@yahoo.co.uk>
2020-03-23 16:06:49 +0000

    top-ups the balance by the top-up amount

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,18 @@ require './lib/oystercard.rb'
 
 describe Oystercard do
   it 'has a default balance of 0' do
-    expect(subject.balance).to eq 0
+    expect(subject.balance).to eq (0)
+  end
+
+  describe '#top_up' do
+    it 'top ups the balance by the top-up amount' do
+      subject.top_up(20)
+      expect(subject.balance).to eq(20)
+    end
+
+    it 'tops ups' do
+      expect{ subject.top_up(1) }.to change{ subject.balance }.by 1
+    end
   end
     
 end
