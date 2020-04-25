aafd62102efb2cc3ef377c400d95ec4b05f0496a
shadz22 <shadz22@yahoo.co.uk>
2020-04-25 17:22:57 +0100

    Implement hit points

--- Battle/app.rb
+++ Battle/app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'player'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -15,14 +15,15 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player1_name = $player1.name
-    @player2_name = $player2.name
+    @player1 = $player1
+    @player2 = $player2
     erb(:play)
   end
 
   get '/attack' do
-    @player1_name = $player1.name
-    @player2_name = $player2.name
+    @player1 = $player1
+    @player2 = $player2
+    @player1.attack(@player2)
     erb(:attack)
   end
 
