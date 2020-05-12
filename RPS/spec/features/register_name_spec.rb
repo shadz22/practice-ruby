c5678e647ae87fa1954545696fe2eb111c62eea6
shadz22 <shadz22@yahoo.co.uk>
2020-05-12 18:47:22 +0100

    Create web helper to keep the code DRY

--- RPS/spec/features/register_name_spec.rb
+++ RPS/spec/features/register_name_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 feature 'register player name' do
   it 'shows the player name on the screen' do
-    visit '/'
-    fill_in :player_1_name, with: 'Shadi'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Shadi Vs Computer'
   end
 end
