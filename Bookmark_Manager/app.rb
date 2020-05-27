a6e29023ece1ce9cc6d95ed9f169b9e152fcfcd6
shadz22 <shadz22@yahoo.co.uk>
2020-05-27 16:03:52 +0100

    see the bookmarks - hardcoded

--- Bookmark_Manager/app.rb
+++ Bookmark_Manager/app.rb
@@ -5,6 +5,15 @@ class BookmarkManager < Sinatra::Base
     "Bookmark Manager"
   end
 
+  get '/bookmarks' do
+    bookmarks = [
+    'http://www.google.com',
+    'http://www.bbc.co.uk',
+    'http://www.msn.com'
+    ]
+    bookmarks.join
+  end
+
   run! if app_file == $0
 end
   
--- Bookmark_Manager/spec/features/viewing_bookmarks_spec.rb
+++ Bookmark_Manager/spec/features/viewing_bookmarks_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 feature 'Viewing bookmarks' do
+
   scenario 'visit the index page' do
     visit '/'
     expect(page).to have_content 'Bookmark Manager'
+  end
+
+  scenario 'see the list of bookmarks' do
+    visit '/bookmarks'
+    expect(page).to have_content 'http://www.google.com'
+    expect(page).to have_content 'http://www.bbc.co.uk'
+    expect(page).to have_content 'http://www.msn.com'
   end
 end
