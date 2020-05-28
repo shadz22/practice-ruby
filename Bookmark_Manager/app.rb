2d9bb0ab568b9e295dd0ad9ea015d24824ee04bb
shadz22 <shadz22@yahoo.co.uk>
2020-05-28 15:07:08 +0100

    Set up bookmark_manager database and create bookmarks table

--- Bookmark_Manager/app.rb
+++ Bookmark_Manager/app.rb
@@ -3,14 +3,12 @@ require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    "Bookmark Manager"
+    erb :index
   end
 
   get '/bookmarks' do
-    
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
-    
   end
 
   run! if app_file == $0
