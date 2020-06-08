bc2b491aa78705ddebfbeae71c4536100879d25a
shadz22 <shadz22@yahoo.co.uk>
2020-06-08 17:07:34 +0100

    Extracting the database connection logic to an object by creating the databse_connection file

---
+++ Bookmark_Manager/spec/database_connection_spec.rb
@@ -0,0 +1,9 @@
+require 'database_connection'
+
+describe '.setup' do
+  it 'creates a connection to the database via PG' do
+    expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
+
+    DatabaseConnection.setup('bookmark_manager_test')
+  end
+end
